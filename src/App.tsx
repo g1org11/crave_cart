b61e0edbb5588b4995fbcebc0b10e6b8f2577637
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-12 14:30:27 +0400

    add global styles and default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 
 import "./App.css";
+import GlobalStyles from "./GlobalStyling";
 import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Main />
     </div>
   );
---
+++ src/defaultTheme.tsx
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colors: {
+    white: "#FFF",
+    black: "#000",
+    red: "#CC3333",
+    blue: "#2A435D",
+    green: "#4BFF3C",
+    floralwhite: "#FFF8EE",
+    lightred: "#FFDFDF",
+  },
+  background: "#FFF8EE",
+  fonts: {
+    primary: "'Roboto', sans-serif",
+  },
+};
