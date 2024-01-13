855e5721dfb09bb661f7035b4aa3381ad265072b
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-13 18:31:35 +0400

    add modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
 import { defaultTheme } from "../defaultTheme";
 import watch from "../assets/header/watch_icon.svg";
 import phone from "../assets/header/phone_icon.svg";
 import logo from "../assets/header/logo.png";
 import cart from "../assets/header/cart.svg";
 import moto from "../assets/header/moto.png";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
+interface IconProps extends FontAwesomeIconProps {
+  show?: boolean;
+}
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div>
       <TopHader>
@@ -24,7 +35,10 @@ const Header = () => {
         <a href="">REGISTER</a>
       </TopHader>
       <MainHeader>
-        <img src={logo} alt="logo" />
+        <a href="/">
+          <img src={logo} alt="logo" />
+        </a>
+
         <NavAndInfo>
           <Ul>
             <li>
@@ -56,6 +70,36 @@ const Header = () => {
               <img src={moto} alt="moto" />
             </CartInfos>
           </CartPart>
+          <Icons>
+            <BurgerIcon icon={faBars} size="2x" onClick={toggleMenu} show={!showMenu} />
+            {showMenu && (
+              <XmarkIcon icon={faXmark} size="2x" onClick={toggleMenu} show={showMenu} />
+            )}
+            {showMenu && (
+              <ModalDiv>
+                <div>
+                  <ModalUl>
+                    <li>
+                      <a href="">HOME</a>
+                    </li>
+                    <li>
+                      <a href="">ABOUT</a>
+                    </li>
+                    <li>
+                      <a href="">ITEMS</a>
+                    </li>
+                    <li>
+                      <a href="">PAGES</a>
+                    </li>
+                    <li>
+                      <a href="">CONTACT</a>
+                    </li>
+                  </ModalUl>
+                </div>
+              </ModalDiv>
+            )}
+          </Icons>
+
           <Login href="">LOGIN</Login>
         </NavAndInfo>
       </MainHeader>
@@ -70,6 +114,9 @@ const TopHader = styled.div`
   justify-content: space-between;
   background-color: ${defaultTheme.colors.red};
   padding: 8px 100px;
+  @media (max-width: 795px) {
+    padding: 8px 50px;
+  }
   a {
     font-size: 12px;
     font-style: normal;
@@ -84,6 +131,9 @@ const Information = styled.div`
   align-items: center;
   justify-content: center;
   gap: 67px;
+  @media (max-width: 795px) {
+    gap: 40px;
+  }
 `;
 const Parts = styled.div`
   display: flex;
@@ -105,6 +155,10 @@ const MainHeader = styled.div`
   justify-content: space-between;
   padding: 19px 100px 25px 100px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  position: relative;
+  @media (max-width: 795px) {
+    padding: 19px 50px 25px 50px;
+  }
 `;
 const NavAndInfo = styled.div`
   display: flex;
@@ -117,6 +171,9 @@ const Ul = styled.ul`
   align-items: center;
   justify-content: center;
   gap: 40px;
+  @media (max-width: 1150px) {
+    display: none;
+  }
   li {
     list-style-type: none;
   }
@@ -136,6 +193,10 @@ const CartPart = styled.div`
 `;
 const CartInfos = styled(CartPart)`
   margin-right: 44px;
+  @media (max-width: 795px) {
+    display: none;
+  }
+
   h3 {
     font-size: 20px;
     font-style: normal;
@@ -185,3 +246,66 @@ const Login = styled.a`
   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.25);
   text-decoration: none;
 `;
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+const BurgerIcon = styled(FontAwesomeIcon)<IconProps>`
+  display: none;
+
+  @media (max-width: 1150px) {
+    display: ${(props) => (props.show ? "inline-block" : "none")};
+  }
+`;
+
+const XmarkIcon = styled(FontAwesomeIcon)<IconProps>`
+  display: none;
+
+  @media (max-width: 1150px) {
+    display: ${(props) => (props.show ? "inline-block" : "none")};
+  }
+`;
+
+const ModalUl = styled.ul`
+  list-style-type: none;
+  margin-bottom: 10px;
+`;
+
+const ModalDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 100%;
+  left: 0%;
+  padding: 20px;
+  background-color: ${defaultTheme.colors.lightred};
+  text-align: center;
+  a {
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-decoration: none;
+    color: ${defaultTheme.colors.blue};
+    margin-bottom: 10px;
+  }
+`;
+
+// const ModalLi = styled.li`
+//   font-size: 12px;
+//   font-style: normal;
+//   font-weight: 300;
+//   line-height: normal;
+//   color: ${defaultTheme.colors.black};
+//   margin-bottom: 8px;
+
+//   a {
+//     text-decoration: none;
+//     /* color: ${defaultTheme.colors.brown}; */
+//     cursor: pointer;
+//   }
+// `;
