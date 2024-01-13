32b2fccd51e84083aee8646960c0f745cd587b94
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-13 15:15:28 +0400

    add top part of heder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import { defaultTheme } from "../defaultTheme";
+import watch from "../assets/header/watch_icon.svg";
+import phone from "../assets/header/phone_icon.svg";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div>
+      <TopHader>
+        <Information>
+          <Parts>
+            <img src={watch} alt="watch" />
+            <p>7.30 AM - 9.30 PM</p>
+          </Parts>
+          <Parts>
+            <img src={phone} alt="phone" />
+            <p>+880 1630 225 015</p>
+          </Parts>
+        </Information>
+        <a href="">REGISTER</a>
+      </TopHader>
+    </div>
+  );
 };
 export default Header;
+
+const TopHader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${defaultTheme.colors.red};
+  padding: 8px 100px;
+  a {
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: ${defaultTheme.colors.floralwhite};
+    text-decoration: none;
+  }
+`;
+const Information = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 67px;
+`;
+const Parts = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: ${defaultTheme.colors.floralwhite};
+    margin-left: 10px;
+  }
+`;
