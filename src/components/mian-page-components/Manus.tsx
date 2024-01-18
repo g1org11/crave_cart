fd2e01b74f32de796323ca239dbf1f60301b7747
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-18 16:08:43 +0400

    add breackfast manus array

--- src/components/mian-page-components/Manus.tsx
+++ src/components/mian-page-components/Manus.tsx
@@ -1,8 +1,55 @@
 import React from "react";
 import styled from "styled-components";
 import { defaultTheme } from "../../defaultTheme";
+import { useState } from "react";
 
 const Manus = () => {
+  const [activeManu, setActiveManu] = useState("BREAKFAST");
+
+  const handleHeaderClick = (manu) => {
+    setActiveRegion(manu);
+  };
+  const ManuCards = {
+    BREAKFAST: [
+      { img: "img" },
+      {
+        meal: "Gazpacho Garlic",
+        material: "Chilled tomato, cucumber, garlic, red pepper soup.",
+        price: "$55",
+      },
+      {
+        meal: "Pan Con Berenjina Frita",
+        material: "Marinated seabass with sour sauce, dill & coriander on bread.",
+        price: "$40",
+      },
+      {
+        meal: "New Lubina Marinada",
+        material: "Fried aubergine with almond on bread (2 pieces).",
+        price: "$45",
+      },
+      {
+        meal: "Gazpacho",
+        material: "Atlantic cod fillet, chips, salad, tartare, lemon.",
+        price: "$35",
+      },
+      {
+        meal: "Coconut Chia Bowl",
+        material: "Marinated seabass with sour saucel & coriander on bread.",
+        price: "$95",
+      },
+      {
+        meal: "New Lubina Marinada",
+        material: "Atlantic cod fillet, chips, salad, tartare, lemon.",
+        price: "$120",
+      },
+      {
+        meal: "Gazpacho Garlic",
+        material: "Chilled tomato, cucumber, garlic, red pepper soup.",
+        price: "$29",
+      },
+    ],
+  };
+
   return (
     <ManuContainer>
       <Wrapper>
@@ -64,4 +111,17 @@ const Navigation = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  p {
+    width: 300px;
+    height: 88px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: ${defaultTheme.colors.red};
+    cursor: pointer;
+  }
 `;
