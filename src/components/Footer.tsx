9fe95e2145ab52ef7a8b5fa47d1b830d3264ed9f
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-25 15:32:59 +0400

    add styling on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import watch from "../assets/footer/watch.svg";
 import phone from "../assets/footer/phone.svg";
+import styled from "styled-components";
+import { defaultTheme } from "../defaultTheme";
 
 const Footer = () => {
   return (
-    <div>
+    <FooterContainer>
       <h1>Our Branch</h1>
-      <div>
-        <div>
+      <Information>
+        <Cards>
           <h2>Robert Food</h2>
           <p>1986 Hilltop DriveBorger, TX 79007</p>
-          <div>
+          <Contact>
             <div>
               <img src={watch} alt="watch" />
               <span>7.30 AM - 9.30 PM</span>
@@ -19,13 +21,13 @@ const Footer = () => {
               <img src={phone} alt="phone" />
               <span>+880 1630-225015</span>
             </div>
-          </div>
+          </Contact>
           <a href="#">Click to View Google Map</a>
-        </div>
-        <div>
+        </Cards>
+        <Cards>
           <h2>Mark A. Reed Food</h2>
           <p>4877 Rose AvenueNew Orleans, LA 70112</p>
-          <div>
+          <Contact>
             <div>
               <img src={watch} alt="watch" />
               <span>7.30 AM - 9.30 PM</span>
@@ -34,13 +36,13 @@ const Footer = () => {
               <img src={phone} alt="phone" />
               <span>+880 1630-225015</span>
             </div>
-          </div>
+          </Contact>
           <a href="#">Click to View Google Map</a>
-        </div>
-        <div>
+        </Cards>
+        <Cards>
           <h2>Mark A. Reed Food</h2>
           <p>1509 Peaceful LaneCleveland, OH 44115</p>
-          <div>
+          <Contact>
             <div>
               <img src={watch} alt="watch" />
               <span>7.30 AM - 9.30 PM</span>
@@ -49,12 +51,102 @@ const Footer = () => {
               <img src={phone} alt="phone" />
               <span>+880 1630-225015</span>
             </div>
-          </div>
+          </Contact>
           <a href="#">Click to View Google Map</a>
-        </div>
-      </div>
-      <p>Copyright © 2022 | Khoshtaria Giorgi</p>
-    </div>
+        </Cards>
+      </Information>
+      <CopyDiv>
+        <CopyRight>Copyright © 2022 | Khoshtaria Giorgi</CopyRight>
+      </CopyDiv>
+    </FooterContainer>
   );
 };
 export default Footer;
+
+const FooterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 85px;
+  h1 {
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${defaultTheme.colors.blue};
+    margin-bottom: 35px;
+  }
+`;
+
+const Information = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 37px 100px 17px 100px;
+  background-color: ${defaultTheme.colors.red};
+`;
+
+const Cards = styled.div`
+  text-align: center;
+  h2 {
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${defaultTheme.colors.floralwhite};
+    margin-bottom: 13px;
+  }
+  P {
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${defaultTheme.colors.floralwhite};
+    margin-bottom: 15px;
+  }
+  a {
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration: none;
+    color: ${defaultTheme.colors.green};
+  }
+`;
+const Contact = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 23px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 16px;
+    span {
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      color: ${defaultTheme.colors.floralwhite};
+      margin-left: 15px;
+    }
+  }
+`;
+const CopyDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 45px 100px 32px 100px;
+  width: 100%;
+  background-color: ${defaultTheme.colors.blue};
+`;
+const CopyRight = styled.p`
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+
+  color: ${defaultTheme.colors.floralwhite};
+`;
