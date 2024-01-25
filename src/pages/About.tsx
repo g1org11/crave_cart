3591f82b34f2ac6491ef1d8c0453dfaf1fbfcbdf
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-25 19:36:26 +0400

    add blog seqcion on about us page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutHero from "../components/about-us-component/AboutHero";
+import AboutBlog from "../components/about-us-component/AboutBlog";
 const About = () => {
   return (
     <div>
       <AboutHero />
+      <AboutBlog />
     </div>
   );
 };
