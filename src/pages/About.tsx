5f657a393aad5a17b0664756c64da0104b3b9299
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-25 16:00:58 +0400

    add routers

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const About = () => {
+  return <div> this is about us seqcion</div>;
+};
+export default About;
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Contact = () => {
+  return <div> this is contact seqcion</div>;
+};
+export default Contact;
