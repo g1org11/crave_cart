6c282bea8a168ec8c503cc23021d84506ef83580
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-01-26 17:16:18 +0400

    add chef section on about us page

---
+++ src/components/about-us-component/Chef.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import chefimg from "../../assets/about-us/chef.png";
+import styled from "styled-components";
+import { defaultTheme } from "../../defaultTheme";
+
+const Chef = () => {
+  return (
+    <ChefContainer>
+      <div>
+        <h2>TASTY AND CRUNCHY</h2>
+        <h1>Our Chef</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidition
+          ullamco laboris nisi ut aliquip ex ea commodo condor consectetur adipiscing elit, sed do
+          eiusmod tempor incidition ullam.
+        </p>
+        <LinkDiv>
+          <Link to="/items">VIEW OUR ALL MANU</Link>
+        </LinkDiv>
+      </div>
+      <img src={chefimg} alt="chefimg" />
+    </ChefContainer>
+  );
+};
+
+export default Chef;
+
+const ChefContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 15px 100px;
+  margin-top: 30px;
+  background-color: ${defaultTheme.colors.lightred};
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+  @media (max-width: 795px) {
+    padding: 15px 50px;
+  }
+
+  h2 {
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: ${defaultTheme.colors.red};
+  }
+  h1 {
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${defaultTheme.colors.blue};
+    margin-bottom: 10px;
+  }
+  p {
+    max-width: 600px;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    color: ${defaultTheme.colors.blue};
+    margin-bottom: 32px;
+  }
+`;
+const LinkDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  transform: translateX(280px);
+
+  a {
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration: none;
+    background-color: ${defaultTheme.colors.red};
+    color: ${defaultTheme.colors.floralwhite};
+    padding: 15px 29px;
+    border-radius: 20px;
+  }
+  @media (max-width: 1050px) {
+    transform: translateX(150px);
+  }
+  @media (max-width: 900px) {
+    transform: translateX(0px);
+  }
+`;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,12 +2,14 @@
 import AboutHero from "../components/about-us-component/AboutHero";
 import AboutBlog from "../components/about-us-component/AboutBlog";
 import OurStory from "../components/about-us-component/OurStory";
+import Chef from "../components/about-us-component/Chef";
 const About = () => {
   return (
     <div>
       <AboutHero />
       <AboutBlog />
       <OurStory />
+      <Chef />
     </div>
   );
 };
