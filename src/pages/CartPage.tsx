eda0c14b64acf8594bd68b01adcc0b118f48b110
g1org11 <99177635+g1org11@users.noreply.github.com>
2024-03-28 16:11:12 +0400

    add  payment  markup

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,11 +1,13 @@
 import CartHero from "../components/cart-components/CartHero";
 import Cart from "../components/cart-components/Cart";
+import Payment from "../components/cart-components/Payment";
 
 const CartPage = () => {
   return (
     <div>
       <CartHero />
       <Cart />
+      <Payment />
     </div>
   );
 };
